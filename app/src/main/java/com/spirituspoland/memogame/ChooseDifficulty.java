c5b5ecb0f07a6478d4a35778650fd8cc480d470c
SpiritusPoland <lkszmalinowski@gmail.com>
2020-09-11 02:27:56 +0200

    created project

---
+++ app/src/main/java/com/spirituspoland/memogame/ChooseDifficulty.java
@@ -0,0 +1,27 @@
+package com.spirituspoland.memogame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_dificulty);
+    }
+
+    private void startEasyGame() {
+
+    }
+
+    private void startMediumGame() {
+
+    }
+
+
+    private void startHardGame() {
+
+    }
+}
---
+++ app/src/main/java/com/spirituspoland/memogame/CustomGame.java
@@ -0,0 +1,14 @@
+ package com.spirituspoland.memogame;
+
+ import android.os.Bundle;
+
+ import androidx.appcompat.app.AppCompatActivity;
+
+ public class CustomGame extends AppCompatActivity {
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+     }
+ }
