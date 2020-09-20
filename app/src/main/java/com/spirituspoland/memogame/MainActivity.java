21507efc50815499b12e582068b478fb545a58c0
SpiritusPoland <lkszmalinowski@gmail.com>
2020-09-20 21:09:04 +0200

    first changes

--- app/src/main/java/com/spirituspoland/memogame/MainActivity.java
+++ app/src/main/java/com/spirituspoland/memogame/MainActivity.java
@@ -2,13 +2,22 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startGame(View view)
+    {
+        Intent intent = new Intent(this, ChooseDifficulty.class);
+        startActivity(intent);
     }
 }
